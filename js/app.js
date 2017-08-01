38ba4e205cee49220a62c9c7021274a5285dd040
Tristan Bayfield <tristan.bayfield@sfu.ca>
2017-08-01 06:58:34 -0700

    Fix slider image styling

--- js/app.js
+++ js/app.js
@@ -1,13 +1,31 @@
+function centerImages() {
+  $('.flexslider').each(function() {
+    var maxHeight = 0;
+    $(this).find('.slides li img').each(function() {
+      maxHeight = Math.max(maxHeight, $(this).height());
+    })
+    $(this).find('.slides li img').each(function() {
+      var img = $(this);
+      var height = img.height();
+      if (height < maxHeight) {
+        img.css('padding-top', 0.5*(maxHeight - height));
+      }
+    })
+  });
+}
 
 $(document).ready(function() {
   $('.flexslider').flexslider({
     slideshow: false,
     controlNav: "thumbnails",
     smoothHeight: false,
+    start: function() { centerImages(); }
   });
-
   $('.swipebox').swipebox({
     removeBarsOnMobile : false,
     hideBarsDelay : 0
+  });
+  $(window).resize(function() {
+    centerImages();
   });
 });
